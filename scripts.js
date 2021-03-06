200a6f1576c4a5586dc9088c1619e65289ed6988
Unchell57 <62289195+Unchell57@users.noreply.github.com>
2021-03-06 11:09:52 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/*$(document).ready(function() {
+
+	$('.container').isotope({
+		itemSelector: '.post',
+				
+		masonry: {
+			columnWidth: 360,
+			singleMode: true,
+			fitWidth: true,
+		},
+	});
+});*/
